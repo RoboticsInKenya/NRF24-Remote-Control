53c03e6b0a90f316036ec7c0b168900168c916ac
RoboticsInKenya <67280188+RoboticsInKenya@users.noreply.github.com>
2020-07-17 01:35:48 +0300

    Update NRF24_BI_DIRECTION_RECEIVE.ino

--- NRF24_BI_DIRECTION_RECEIVE/NRF24_BI_DIRECTION_RECEIVE.ino
+++ NRF24_BI_DIRECTION_RECEIVE/NRF24_BI_DIRECTION_RECEIVE.ino
@@ -1,3 +1,14 @@
+//copyright reserve @ Robotics in Kenya
+//For Subscribe to My Youtube Channe, Robotics In Kenya
+//Reach Me:
+// FaceBook: Robotics In Kenya
+// Instagram: robotics_in_kenya
+//Whatsapp: +254700670999
+//email: roboticsinkenya@gmail.com
+//This code utilizes PID to monitor Motor Speed
+//and drive it at a constant speed iregadless of the Load
+//
+
 
 #include <SPI.h>
 #include <nRF24L01.h>
